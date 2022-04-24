1152889285467e551896124a407387d714bb8382
王景 <asd5196a8@qq.com>
2022-04-24 09:47:57 +0800

    chore: add vconsole tool

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
 const TransformPages = require('uni-read-pages');
+const VConsolePlugin = require('vconsole-webpack-plugin');
 
 const { webpack } = new TransformPages();
 module.exports = {
   transpileDependencies: ['uview-ui'],
   configureWebpack: {
     plugins: [
+      new VConsolePlugin({
+        enable: process.env.NODE_ENV === 'development',
+      }),
       new webpack.DefinePlugin({
         ROUTES: webpack.DefinePlugin.runtimeValue(() => {
           const tfPages = new TransformPages({
